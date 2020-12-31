8a1d8fa3fefb315a39a399fd6064c449e1cee98e
jaybaybremner <j.bremner@ymail.com>
2020-12-31 17:06:54 +0000

    adding examples

--- script.js
+++ script.js
@@ -30,5 +30,24 @@ var app = new Vue({
       }
   })
 
-  app4.todos.push({ text: 'hello world' });
+  var app5 = new Vue({
+      el: '#app-5',
+      data: {
+          message: 'Hello Vue.js'
+      },
+      methods: {
+          reverseMessage: function () {
+              this.message = this.message.split('').reverse().join('')
+          }
+      }
+  })
+
+  var app6 = new Vue({
+      el: '#app-6',
+      data: {
+          message: 'Hello Vue'
+      }
+  })
+
+
 
